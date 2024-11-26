09e20e0bf829d92a9099ea383ec8206c2458f8ed
matheus.souza <matheus.souza@dataprom.com>
2024-11-26 12:02:12 -0300

    Adicionando estrutura inicial do projeto ao repositório

---
+++ src/main/java/br/com/ecommerce/api/DesafioEcommerceBackendApplication.java
@@ -0,0 +1,13 @@
+package br.com.ecommerce.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioEcommerceBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioEcommerceBackendApplication.class, args);
+	}
+
+}
